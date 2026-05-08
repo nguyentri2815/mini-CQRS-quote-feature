ee2064908ed609faefb27fe9280e4364f08584c9
Tri Nguyen <tri.nguyen@alpaca.vn>
2026-05-08 19:55:51 +0700

    Spring Initializr

---
+++ src/test/java/com/example/quoteservice/QuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
